9c0b11fb11a54cdaad77869fb901c42381550d75
s20owen <71678206+s20owen@users.noreply.github.com>
2026-05-10 09:31:40 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_CACHE_VERSION = "2026.05.09-v2";
+const APP_CACHE_VERSION = "2026.05.09-v3";
 const CACHE_NAME = `putt-mobile-cache-${APP_CACHE_VERSION}`;
 const CORE_APP_SHELL = [
   "./",
